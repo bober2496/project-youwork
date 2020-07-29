fddaf0f82c22c3d758183da13ab925c2e27ee35f
Bober <bober0123@gmail.com>
2020-07-29 13:53:39 +0200

    Office started

---
+++ youwork/Assets/Bober/Scripts/MainCamera_script.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+
+public class MainCamera_script : MonoBehaviour
+{
+    //GameObject references
+    private Transform mainCamTransform;
+    public Transform _mainCameraFollowing;
+
+    //UI
+    [SerializeField] private float Smoothness = 0.2f;
+    [SerializeField] private Vector3 cameraOffset = new Vector3(0f, 0f, 10f);
+
+    private void Awake()
+    {
+        mainCamTransform = GetComponent<Transform>();
+    }
+    private void Start()
+    {
+        _mainCameraFollowing = GameObject.Find("RageMan").GetComponent<Transform>();
+    }
+
+    void Update()
+    {
+        mainCamTransform.position = Vector3.Lerp(mainCamTransform.position, _mainCameraFollowing.position + cameraOffset, Smoothness);
+    }
+}
